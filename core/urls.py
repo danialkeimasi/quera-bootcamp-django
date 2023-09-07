9a780d9ecd01e8c59a2ad294f9d935bdce966a30
Danial Keimasi <danialkeimasi@gmail.com>
2023-09-07 23:35:24 +0330

    Add examples for urls with builtin path converters

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index_view, name="index"),
+    path("hello/<str:name>", views.hello_view, name="hello"),
+    path("sum/<int:a>/<int:b>", views.sum_view, name="sum"),
+    path("slug/<slug:slug>", views.slug_view, name="slug"),
+    path("uuid/<uuid:uuid>", views.uuid_view, name="uuid"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import HttpResponse
+from uuid import UUID
+
 from django.http import HttpRequest
+from django.shortcuts import HttpResponse
 
 
-def index(request: HttpRequest):
-    return HttpResponse("Hello, world. You're at the core index.")
+def index_view(request: HttpRequest):
+    return HttpResponse(f"Hello, world. You're at the {request.path_info}")
+
+
+def hello_view(request: HttpRequest, name: str):
+    return HttpResponse(f"Hello, {name.title()}. You're at the {request.path_info}")
+
+
+def sum_view(request: HttpRequest, a: int, b: int):
+    return HttpResponse(f"{a} + {b} = {a + b}")
+
+
+def slug_view(request: HttpRequest, slug: str):
+    return HttpResponse(f"Slug: {slug}")
+
+
+def uuid_view(request: HttpRequest, uuid: UUID):
+    return HttpResponse(f"UUID: {uuid}")
